5057ad57affd7c772effb0a2434e349d44401344
Jarred Stone <81976856+JarredStone37@users.noreply.github.com>
2023-04-07 22:14:26 +0000

    updated to make into chiropractor app

--- 03 chatgpt chat assistant website.py
+++ 03 chatgpt chat assistant website.py
@@ -1,9 +1,9 @@
 import openai
 import gradio
 
-openai.api_key = "####"
+openai.api_key = "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"
 
-messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
+messages = [{"role": "system", "content": "You are a chiropractor that specializes in expplaining complex concepts to people like they're five"}]
 
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
@@ -15,6 +15,6 @@ def CustomChatGPT(user_input):
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
-demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Real Estate Pro")
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "YOTO Chiro")
 
 demo.launch(share=True)
